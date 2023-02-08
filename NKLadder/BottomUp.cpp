fe3e079191d1b13a3a3a6bc50f629f48c97fd98c
pratapshingane18 <sushrutrao465@gmail.com>
2023-02-08 16:24:27 +0530

    NK Ladder

---
+++ NKLadder/BottomUp.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+// Bottom Up
+// O(KN) Square Complexity
+int CountWaysTD(int n, int k,int *dp){
+    int ans=0;
+    if(n ==0){
+        return 1;
+    }
+    if(n <0){
+        return 0;
+    }
+
+    if(dp[n] != 0){
+        return dp[n];
+    }
+
+    for(int jump=1; jump <= k; jump++){
+        ans += CountWaysTD(n-jump,k,dp);
+    }
+
+    return ans;
+}
+
+int main(){
+    int n,k;
+    cin>>n>>k;
+    int dp[1000]={0};
+
+    cout<<"Number of Ways are: "<<CountWaysTD(n,k,dp)<<endl;
+
+    return 0;
+}
